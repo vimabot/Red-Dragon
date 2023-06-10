d48643d0d93080a80f9aac4fd7baf48caca77f68
vimabot <136125895+vimabot@users.noreply.github.com>
2023-06-10 18:25:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'your key',
 }
 
-global.alivemsg = `📤I am alive now😇
-🤷‍♀️How can I help you?😉` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `👋Hello!
+I am online 😜, how can i help you 🤔?` //Costomize Alive Message (type your message in `` )
 
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
@@ -29,10 +29,10 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
-global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
+global.vcardowner = ['+94751255279'] // owner number
+global.ownername = "𝙼𝚁.𝚅𝙸𝙼𝚄𝙺𝚃𝙷𝙸😈" // owner name
+global.ytname = "AnonymouS BasS" // yt chanel name
+global.socialm = "Instagram: v_h_e_w_a_g_e" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94751255279']
+global.ownertag = '+94751255279' //your tag number
+global.botname = '𝚅𝙸𝙼𝙰 - 𝙱𝙾𝚃' //ur bot name
+global.packname = "𝚅𝙸𝙼𝙰 - 𝙱𝙾𝚃" // sticker packname
+global.author = "𝙼𝚁.𝚅𝙸𝙼𝚄𝙺𝚃𝙷𝙸😈" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ 𝚅𝙸𝙼𝙰 - 𝙱𝙾𝚃 ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94751255279'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
